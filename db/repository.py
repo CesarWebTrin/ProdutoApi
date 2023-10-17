1cff51aac7adea46b6f7c2337c891652f121a225
CesarWebTrin <cesinhart@gmail.com>
2023-10-16 21:29:17 -0300

    FIRST COMMIT PROJETO CONCLUIDO

--- db/repository.py
+++
@@ -1,8 +0,0 @@
-import pyodbc
-
-def _conn():
-    conn = pyodbc.connect("Driver={SQL Server Native Client 11.0};"
-                        "Server=DELL;"
-                        "Database=DATA_IMPACTA;"
-                        "Trusted_Connection=yes;")
-    
